919b1ce8671ee9223c17850d38f285c4c8dea643
jhd7130 <jhd7130@github.com>
2025-01-14 02:27:53 +0900

    refactor : 금액 계산 로직 분리

--- refactoring2nd/src/main/java/org/example/chapter01/code/Statment.java
+++ refactoring2nd/src/main/java/org/example/chapter01/code/Statment.java
@@ -7,25 +7,7 @@ public class Statment {
     StringBuilder result = new StringBuilder(String.format("청구내역 (고객명: %s)\n", invoice.getCustomer()));
     for (Performance performance : invoice.getPerformances()) {
       Play play = plays.get(performance);
-      int thisAmount = 0;
-
-      switch (play.getType()) {
-        case TRAGEDY:
-          thisAmount = 40000;
-          if (performance.getAudience() > 30) {
-            thisAmount += 1000 * (performance.getAudience() - 30);
-          }
-          break;
-        case COMEDY:
-          thisAmount = 30000;
-          if (performance.getAudience() > 20) {
-            thisAmount += 10000 + 500 * (performance.getAudience() - 20);
-          }
-          thisAmount += 300 * performance.getAudience();
-          break;
-        default:
-          throw new Exception("알 수 없는 장르");
-      }
+      int thisAmount = amountFor(performance, play);
 
       // 포인트를 적립한다.
       volumeCredit += Math.max(performance.getAudience() - 30, 0);
@@ -43,5 +25,28 @@ public class Statment {
     result.append(String.format("총액: $%d\n",totalAmount / 100));
     result.append(String.format("적립 포인트: %d점", volumeCredit));
     return result.toString();
+  }
+
+  private int amountFor(Performance performance, Play play) throws Exception {
+    int thisAmount = 0;
+
+    switch (play.getType()) {
+      case TRAGEDY:
+        thisAmount = 40000;
+        if (performance.getAudience() > 30) {
+          thisAmount += 1000 * (performance.getAudience() - 30);
+        }
+        break;
+      case COMEDY:
+        thisAmount = 30000;
+        if (performance.getAudience() > 20) {
+          thisAmount += 10000 + 500 * (performance.getAudience() - 20);
+        }
+        thisAmount += 300 * performance.getAudience();
+        break;
+      default:
+        throw new Exception("알 수 없는 장르");
+    }
+    return thisAmount;
   }
 }
