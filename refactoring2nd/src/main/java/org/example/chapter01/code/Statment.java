66dc3822214fcbeeb510af715545aa74e8365772
jhd7130 <jhd7130@github.com>
2025-01-15 02:10:50 +0900

    feature : 보너스포인트 분리

--- refactoring2nd/src/main/java/org/example/chapter01/code/Statment.java
+++ refactoring2nd/src/main/java/org/example/chapter01/code/Statment.java
@@ -9,13 +9,7 @@ public class Statment {
 
       int thisAmount = amountFor(performance, plays);
 
-      // 포인트를 적립한다.
-      volumeCredit += Math.max(performance.getAudience() - 30, 0);
-
-      // 희극 관객 5명마다 추가 포인트를 제공핟나.
-      if (playFor(plays, performance).getType().equals(PlayType.COMEDY)) {
-        volumeCredit += Math.floor(performance.getAudience() / 5);
-      }
+      volumeCredit = volumeCreditFor(plays, performance);
 
       // 청구 내역을 출력한다.
       result.append(String.format("%s: $%d %d석\n",playFor(plays, performance).getName(), thisAmount / 100, performance.getAudience()));
@@ -27,29 +21,41 @@ public class Statment {
     return result.toString();
   }
 
+  private int volumeCreditFor(Plays plays, Performance performance) {
+    int result = 0;
+    // 포인트를 적립한다.
+    result += Math.max(performance.getAudience() - 30, 0);
+
+    // 희극 관객 5명마다 추가 포인트를 제공핟나.
+    if (playFor(plays, performance).getType().equals(PlayType.COMEDY)) {
+      result += Math.floor(performance.getAudience() / 5);
+    }
+    return result;
+  }
+
   private Play playFor(Plays plays, Performance performance) {
     return plays.get(performance);
   }
 
   private int amountFor(Performance performance, Plays plays) throws Exception {
-    int thisAmount = 0;
+    int result = 0;
     switch (playFor(plays, performance).getType()) {
       case TRAGEDY:
-        thisAmount = 40000;
+        result = 40000;
         if (performance.getAudience() > 30) {
-          thisAmount += 1000 * (performance.getAudience() - 30);
+          result += 1000 * (performance.getAudience() - 30);
         }
         break;
       case COMEDY:
-        thisAmount = 30000;
+        result = 30000;
         if (performance.getAudience() > 20) {
-          thisAmount += 10000 + 500 * (performance.getAudience() - 20);
+          result += 10000 + 500 * (performance.getAudience() - 20);
         }
-        thisAmount += 300 * performance.getAudience();
+        result += 300 * performance.getAudience();
         break;
       default:
         throw new Exception("알 수 없는 장르");
     }
-    return thisAmount;
+    return result;
   }
 }
